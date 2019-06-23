5f5835673b7638d0faae7e1e66dc9d4c6d7e2b97
Abhinandh Ajay <abhinandhajay@gmail.com>
2019-06-23 11:45:48 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -14,6 +14,28 @@ def insert(title, author, year, isbn):
     conn.commit()
     conn.close()
 
+def search(title = "", author = "", year = "", isbn = ""):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?",(title, author, year, isbn))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(id):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM book where id=?",(id,))
+    conn.commit()
+    conn.close()
+
+def update(id, title, author, year, isbn):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE book set title=?, author=?, year=?, isbn=? WHERE id=?",(title, author, year, isbn, id))
+    conn.commit()
+    conn.close()
+
 def view():
     conn = sqlite3.connect("books.db")
     cur = conn.cursor()
@@ -23,5 +45,4 @@ def view():
     return rows
 
 connect()
-insert("Sherlock Holmes", "Sir ACD", 1980, 10983123)
 print(view())
